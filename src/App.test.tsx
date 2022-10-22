4c9df07550e8339e949321a4d13c58a2179c6371
Natalia Solórzano <00120319@uca.edu.sv>
2022-10-21 23:33:54 -0600

    :Sparkles: Add: UI Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,48 @@
-import { describe } from 'vitest'
+import { describe } from 'vitest';
+import App from './App';
+import * as ReactDOM from 'react-dom';
+import React from 'react';
 
-describe('App', () => { 
-    // package.json
-    //"test": "vitest --passWithNoTests"
-    it.todo('should render');
-    /*(it('should render', () => {
+describe('App', () => {
 
-    })*/
+    let container: HTMLDivElement;
+
+    beforeEach(() => {
+        container = document.createElement('div');
+        document.body.appendChild(container);
+        ReactDOM.render(<App />, container);
+    })
+
+    afterEach(() => {
+        document.body.removeChild(container);
+        container.remove();
+    })
+
+    it('should render', () => {
+        const render = document.createElement('div');
+        ReactDOM.render(<App />, render);
+        ReactDOM.unmountComponentAtNode(render);
+    });
+
+    it('should have title "Mi DUI es valido?"', () => {
+        const title = container.querySelectorAll('h1');
+        expect(title[0].textContent).toBe('Mi DUI es valido?');
+
+    });
+
+    it('should have an input text', () => {
+        const input = container.querySelector('input');
+        expect(input?.type).toBe('text');
+    });
+
+
+    it('should have a button with text "Validar"', () => {
+        const button = container.querySelector('button');
+        expect(button?.textContent).toBe('Validar');
+    })
+
+    it('should have a form submission', () => {
+        const form = container.querySelector('form');
+        expect(form);
+    });
 })
